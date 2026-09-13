9bd53f4e3d292f0cabab3c062ca3d0377ed4fa7e
salesconnect91-lab <sales.connect91@gmail.com>
2026-09-13 17:28:15 +0300

    Switch NAVILO to global language runtime

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { AuthProvider } from "./auth/AuthContext";
-import LanguageRuntime from "./components/LanguageRuntime";
+import GlobalLanguageRuntime from "./components/GlobalLanguageRuntime";
 import "./printTargetRuntime";
 import "./accountNameDisplayRuntime";
 import "./languageIsolationRuntime";
@@ -27,7 +27,7 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <LanguageRuntime />
+        <GlobalLanguageRuntime />
         <App />
       </AuthProvider>
     </BrowserRouter>
