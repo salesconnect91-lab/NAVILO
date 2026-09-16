235e123f52741100154280c8024a9a292eb4e643
salesconnect91-lab <sales.connect91@gmail.com>
2026-09-16 14:49:44 +0300

    Wire approval workflow settings

--- src/modules/settings/Settings.tsx
+++ src/modules/settings/Settings.tsx
@@ -5,12 +5,14 @@ import DocumentPrintSettings from "./DocumentPrintSettings";
 import OrderBookSettings from "./OrderBookSettings";
 import GatePassWeighbridgeSettings from "./GatePassWeighbridgeSettings";
 import AccessManagementSettings from "./AccessManagementSettings";
+import ApprovalWorkflowSettings from "./ApprovalWorkflowSettings";
 
 export default function Settings() {
   return (
     <Routes>
       <Route path="/" element={<CompanySettings />} />
       <Route path="/access" element={<AccessManagementSettings />} />
+      <Route path="/approvals" element={<ApprovalWorkflowSettings />} />
       <Route path="/tax" element={<TaxSettings />} />
       <Route path="/documents" element={<DocumentPrintSettings />} />
       <Route path="/order-book" element={<OrderBookSettings />} />
